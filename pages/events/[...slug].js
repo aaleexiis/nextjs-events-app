8f6783895d2f5ceeec0e9db04a5dabbc66d5bc10
aaleexiis <aaleexiis@gmail.com>
2023-03-13 09:02:23 +0100

    Remove unnecessary code

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,7 +2,6 @@ import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
-import { getFilteredEvents } from '../../utils/api';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
@@ -15,7 +14,7 @@ function FilteredEventsPage(props) {
     const filterData = router.query.slug;
 
     const { data, error } = useSWR(
-        'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json',
+        'https://react-udemy-95ed9-default-rtdb.firebaseio.com/events.json',
         (url) => fetch(url).then(res => res.json())
     );
 
@@ -91,53 +90,9 @@ function FilteredEventsPage(props) {
     return (
         <Fragment>
             <ResultsTitle date={date} />
-            <EventList items={filteredEvents} />
+            <EventList events={filteredEvents} />
         </Fragment>
     );
 }
-
-// export async function getServerSideProps(context) {
-//   const { params } = context;
-
-//   const filterData = params.slug;
-
-//   const filteredYear = filterData[0];
-//   const filteredMonth = filterData[1];
-
-//   const numYear = +filteredYear;
-//   const numMonth = +filteredMonth;
-
-//   if (
-//     isNaN(numYear) ||
-//     isNaN(numMonth) ||
-//     numYear > 2030 ||
-//     numYear < 2021 ||
-//     numMonth < 1 ||
-//     numMonth > 12
-//   ) {
-//     return {
-//       props: { hasError: true },
-//       // notFound: true,
-//       // redirect: {
-//       //   destination: '/error'
-//       // }
-//     };
-//   }
-
-//   const filteredEvents = await getFilteredEvents({
-//     year: numYear,
-//     month: numMonth,
-//   });
-
-//   return {
-//     props: {
-//       events: filteredEvents,
-//       date: {
-//         year: numYear,
-//         month: numMonth,
-//       },
-//     },
-//   };
-// }
 
 export default FilteredEventsPage;
